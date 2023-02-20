44c899e246caed9ba94cc9f5b0428cd980f67d5b
Dmitriy Romanov <romanovua1991@gmail.com>
2023-02-20 22:51:44 +0300

    Delete build directory from repository

--- build/components/breadcrumbs.php
+++
@@ -1,6 +0,0 @@
-<div class="nav-breadcrumbs">
-    <ul>
-        <li><a href="javascript:;">Главная страница</a></li>
-        <li><a href="javascript:;">Текущая страница</a></li>
-    </ul>
-</div>
